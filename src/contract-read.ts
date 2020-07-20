9cda42caecaeb41d0279719241e16b4c88d46e66
Sam <hatem@clearrain.xyz>
2020-07-20 11:20:35 -0400

    feat: order tx with nonce so that tx from the same account in the same block execute in order

--- src/contract-read.ts
+++ src/contract-read.ts
@@ -4,35 +4,36 @@ import { retryWithBackoff, batch, softFailWith } from 'promises-tho';
 import { getTag, arrayToHex, unpackTags } from './utils';
 import { execute, ContractInteraction } from './contract-step';
 import { InteractionTx } from './interaction-tx';
+import { decodeJWT } from 'did-jwt'
 
 /**
- * Queries all interaction transactions and replays a contract to its latest state. 
- * 
- * If height is provided, will replay only to that block height. 
- * 
+ * Queries all interaction transactions and replays a contract to its latest state.
+ *
+ * If height is provided, will replay only to that block height.
+ *
  * @param arweave     an Arweave client instance
  * @param contractId  the Transaction Id of the contract
  * @param height      if specified the contract will be replayed only to this block height
  */
 export async function readContract(arweave: Arweave, contractId: string, height = Number.POSITIVE_INFINITY): Promise<any> {
-        
+
   const contractInfo = await loadContract(arweave, contractId);
 
-  let state: any; 
+  let state: any;
   try {
       state = JSON.parse(contractInfo.initState);
   } catch (e) {
       throw new Error(`Unable to parse initial state for contract: ${contractId}`);
   }
-  
-  // Load all the interaction txs relevant to this contract. 
 
-  // This can be made a lot cleaner with some GraphQL features, 
+  // Load all the interaction txs relevant to this contract.
+
+  // This can be made a lot cleaner with some GraphQL features,
   // (block info in results, pagination)
-  // but for now, we stick with arql and use some utils to help 
-  // with concurency and retry on errors. 
-  // (we can be firing off thousands of requests here) 
-  
+  // but for now, we stick with arql and use some utils to help
+  // with concurency and retry on errors.
+  // (we can be firing off thousands of requests here)
+
   const arql = {
       op: 'and',
       expr1: {
@@ -46,41 +47,41 @@ export async function readContract(arweave: Arweave, contractId: string, height 
           expr2: contractId
       }
   }
-  
+
   let transactions = await arweave.arql(arql);
   const getTxInfoFn = retryWithBackoff(
-      { tries: 3, startMs: 1000 }, 
-      (id) => getFullTxInfo(arweave, id)
+      { tries: 3, startMs: 1000 },
+      (id) => getFullTxInfoWNonce(arweave, id)
   );
   const batcher = batch(
     { batchDelayMs: 50, batchSize: 3 },
     softFailWith(undefined, getTxInfoFn)
   )
   console.log(`Query returned ${transactions.length} interactions`);
-  
+
   let unconfirmed = await batcher(transactions);
 
   console.log(`Recieved info for ${unconfirmed.length} transactions`);
-  
-  // Filter out txs that are not confirmed yet, not found, 
+
+  // Filter out txs that are not confirmed yet, not found,
   // or are below the height we are replaying to.
-  
+
   let txInfos = unconfirmed
-    .filter(x => 
-      x && 
-      x.info.confirmed && 
+    .filter(x =>
+      x &&
+      x.info.confirmed &&
       x.info.confirmed.block_height <= height
     ) as InteractionTx[]
-  
+
   console.log(`Replaying ${txInfos.length} confirmed interactions`);
 
   txInfos.sort((a, b) => a.sortKey.localeCompare(b.sortKey));
 
   const { handler, swGlobal } = contractInfo
-  
+
   for (let i = 0; i < txInfos.length; i++) {
       let input;
-      try { 
+      try {
           input = getTag(txInfos[i].tx, 'Input')
           input = JSON.parse(input);
       } catch (e) {}
@@ -89,16 +90,16 @@ export async function readContract(arweave: Arweave, contractId: string, height 
           console.warn(`Skipping tx with missing or invalid Input tag - ${txInfos[i].id}`);
           continue;
       }
-      
+
       const interaction: ContractInteraction = {
         input: input,
         caller: txInfos[i].from,
       }
-      
+
       swGlobal._activeTx = txInfos[i];
 
       const result = await execute(handler, interaction, state);
-      
+
       if (result.type === 'exception') {
         console.warn(`${result.result}`);
         console.warn(`Executing of interaction: ${txInfos[i].id} threw exception.`);
@@ -107,35 +108,35 @@ export async function readContract(arweave: Arweave, contractId: string, height 
         console.warn(`${result.result}`);
         console.warn(`Executing of interaction: ${txInfos[i].id} returned error.`);
       }
-      
+
       state = result.state;
   }
 
-  return state; 
+  return state;
 }
 
 // This gets the full Tx Info, and calcutes a sort key.
 // It needs to get the block_height and indep_hash from
-// the status endpoint as well as the tx itself. Returns 
-// undefined if the transactions is not confirmed. 
+// the status endpoint as well as the tx itself. Returns
+// undefined if the transactions is not confirmed.
 async function getFullTxInfo(arweave: Arweave, id: string): Promise<InteractionTx | undefined> {
   const [tx, info] = await Promise.all([
       arweave.transactions.get(id).catch(e => {
         if (e.type === 'TX_PENDING') {
           return undefined
-        } 
+        }
         throw(e);
       }),
       arweave.transactions.getStatus(id)
   ])
-  
+
   if (!tx || !info || !info.confirmed) {
     return undefined;
   }
- 
+
   // Construct a string that will lexographically sort.
   // { block_height, sha256(block_indep_hash + txid) }
-  // pad block height to 12 digits and convert hash value 
+  // pad block height to 12 digits and convert hash value
   // to a hex string.
   const blockHashBytes = arweave.utils.b64UrlToBuffer(info.confirmed.block_indep_hash)
   const txIdBytes = arweave.utils.b64UrlToBuffer(id)
@@ -144,7 +145,42 @@ async function getFullTxInfo(arweave: Arweave, id: string): Promise<InteractionT
   const block_height = `000000${info.confirmed.block_height}`.slice(-12);
 
   const sortKey = `${block_height},${hashed}`
-  
+
   return { tx, info, id: tx.id, sortKey, from: await arweave.wallets.ownerToAddress(tx.owner) }
 }
 
+async function getFullTxInfoWNonce(arweave: Arweave, id: string): Promise<InteractionTx | undefined> {
+  const [tx, info] = await Promise.all([
+      arweave.transactions.get(id).catch(e => {
+        if (e.type === 'TX_PENDING') {
+          return undefined
+        }
+        throw(e);
+      }),
+      arweave.transactions.getStatus(id)
+  ])
+
+  if (!tx || !info || !info.confirmed) {
+    return undefined;
+  }
+
+  // Construct a string that will lexographically sort.
+  // { block_height, sha256(block_indep_hash + txid) }
+  // pad block height to 12 digits and convert hash value
+  // to a hex string.
+  const blockHashBytes = arweave.utils.b64UrlToBuffer(info.confirmed.block_indep_hash)
+  const txIdBytes = arweave.utils.b64UrlToBuffer(id)
+  const concatted = arweave.utils.concatBuffers([blockHashBytes, txIdBytes])
+  const hashed = arrayToHex(await arweave.crypto.hash(concatted))
+  const block_height = `000000${info.confirmed.block_height}`.slice(-12);
+
+  // Add nonce to sort order so transactions from the same account execute in order
+  let jwt = getTag(tx, 'Input')
+  jwt = jwt.substring(1, jwt.length - 1)
+  const input = decodeJWT(jwt).payload
+  const nonce = input.nonce
+
+  const sortKey = `${block_height},${nonce},${hashed}`
+
+  return { tx, info, id: tx.id, sortKey, from: await arweave.wallets.ownerToAddress(tx.owner) }
+}
