ae19fb5e2ed73175ab68f5f753cd42190a0e259b
Sam <hatem@clearrain.xyz>
2020-08-03 16:28:48 -0400

    cleaned up test

--- test/community-test.js
+++ test/community-test.js
@@ -12,7 +12,6 @@ describe('Miscellaneous functions', function () {
   let MEMBER
 
   let helper
-  let packageNExecute
 
   before(async function () {
     helper = new TestHelper()
@@ -20,8 +19,6 @@ describe('Miscellaneous functions', function () {
     ADMIN = accounts[0]
     MOD = accounts[1]
     MEMBER = accounts[2]
-
-    packageNExecute = helper.packageNExecute.bind(helper)
 
     state = Object.assign({}, fullState)
   })
@@ -40,19 +37,19 @@ describe('Miscellaneous functions', function () {
     })
 
     it('owner can change access', async function () {
-      const res = await packageNExecute(close, state, OWNER)
+      const res = await helper.packageNExecute(close, state, OWNER)
       state = res.state
       assert.equal(state.isOpen, false)
     })
 
     it('admin can change access', async function () {
-      const res = await packageNExecute(open, state, ADMIN)
+      const res = await helper.packageNExecute(open, state, ADMIN)
       state = res.state
       assert.equal(state.isOpen, true)
     })
 
     it('moderator attempt to change access fails', async function () {
-      const res = await packageNExecute(open, state, MOD)
+      const res = await helper.packageNExecute(open, state, MOD)
       assert.equal(res.result, 'Must have admin privileges to set access')
     })
   })
@@ -67,29 +64,29 @@ describe('Miscellaneous functions', function () {
     })
 
     it('anyone can add a child community initially', async function () {
-      const res = await packageNExecute(addInteraction, state, MEMBER)
+      const res = await helper.packageNExecute(addInteraction, state, MEMBER)
       state = res.state
       assert.equal(state.children[OTHER_COMMUNITY], true)
     })
 
     it('fails when someone without admin privileges tries to remove a community', async function () {
-      const res = await packageNExecute(removeInteraction, state, MOD)
+      const res = await helper.packageNExecute(removeInteraction, state, MOD)
       assert.equal(res.result, 'Caller must have admin privileges to remove a community')
     })
 
     it('admin can remove a community', async function () {
-      const res = await packageNExecute(removeInteraction, state, ADMIN)
+      const res = await helper.packageNExecute(removeInteraction, state, ADMIN)
       state = res.state
       assert.equal(state.children[OTHER_COMMUNITY], false)
     })
 
     it('fails to add a community that has been previously removed', async function () {
-      const res = await packageNExecute(addInteraction, state, MOD)
+      const res = await helper.packageNExecute(addInteraction, state, MOD)
       assert.equal(res.result, 'A community that has been removed can only be added back with admin privileges')
     })
 
     it('allows admin to add back a community', async function () {
-      const res = await packageNExecute(addInteraction, state, ADMIN)
+      const res = await helper.packageNExecute(addInteraction, state, ADMIN)
       state = res.state
       assert.equal(state.children[OTHER_COMMUNITY], true)
     })
