3eeae1725938f1289ae646bed6f824cc685012a0
Sam <hatem@clearrain.xyz>
2020-08-06 18:38:31 -0400

    feat: created script to execute contract interactions

---
+++ test/remote/executeDevTransaction.js
@@ -0,0 +1,43 @@
+import { CONTRACT_ID } from 'outpost-protocol'
+import { REMOVE_CHILD } from 'outpost-protocol/functionTypes'
+import { testKeys } from '../helpers/constants'
+import { interactWrite } from '../../src/contract-interact'
+import fs from 'fs'
+import path from 'path'
+import Arweave from 'arweave/node'
+import TestHelper from '../helpers'
+
+require('dotenv').config()
+
+const arweave = Arweave.init({
+  host: 'arweave.net',
+  port: 443,
+  protocol: 'https'
+})
+
+console.log(process.env.DEV_WALLET, 'THE DEV WALLET')
+const devWalletPath = path.resolve(__dirname, process.env.DEV_WALLET)
+const rawWallet = fs.readFileSync(devWalletPath)
+const devWallet = JSON.parse(rawWallet)
+
+// change the contract input for different functions
+const CONTRACT_INPUT = {
+  function: REMOVE_CHILD,
+  communityId: 'El-PaaA_wPx4nJuL9tcriZ6OPVoMOl8shmdgjG3Nr4A'
+}
+
+async function postInteraction () {
+  const helper = new TestHelper(true, CONTRACT_ID)
+  const accounts = await helper.setupEnv(testKeys)
+
+  const NEW_OWNER = accounts[4]
+
+  const jwt = await helper.package(CONTRACT_INPUT, NEW_OWNER)
+
+  const txId = await interactWrite(arweave, devWallet, CONTRACT_ID, jwt)
+
+  console.log(txId, 'THE TXID OF THE TRANSACTION')
+  await helper.stopIPFS()
+}
+
+postInteraction()
