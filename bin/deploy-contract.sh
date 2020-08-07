747d1e397013982df6af6a1e960458248f65d1f9
Sam <hatem@clearrain.xyz>
2020-08-06 20:07:58 -0400

    refactor deploy script

--- bin/deploy-contract.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Pass in key-file location to script
-
-echo "Deploying the contract..."
-
-node smartweave-cli --key-file $1 \
-  --create --contract-src build/community.js \
-  --init-state initialState.json
---
+++ bin/deploy-dev.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const path = require('path')
+const Arweave = require('arweave/node')
+const { createInitState } = require('outpost-protocol')
+const smartweave = require('../src')
+const { OWNER, DEV_NAME, IS_OPEN } = require('../test/helpers/constants')
+
+require('dotenv').config()
+
+const devWalletPath = path.resolve(__dirname, `../${process.env.DEV_WALLET}`)
+const rawWallet = fs.readFileSync(devWalletPath)
+const wallet = JSON.parse(rawWallet)
+
+const srcPath = path.resolve(__dirname, '../build/community.js')
+const contractSrc = fs.readFileSync(srcPath)
+const initState = createInitState(OWNER, DEV_NAME, IS_OPEN)
+
+const arweave = Arweave.init({
+  host: 'arweave.net',
+  port: 443,
+  protocol: 'https'
+})
+
+/*
+smartweave.createContract(arweave, wallet, contractSrc, initState).then(
+  (contractID) => {
+    console.log('Contract created in TX: ' + contractID)
+  }
+)
+*/
